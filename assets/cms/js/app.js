75f4dc21cc2a42da34e288d0cf2e2165e72a36af
GotPop <liam@gotpop.net>
2021-05-22 19:52:50 +0100

    Env check

--- assets/cms/js/app.js
+++ assets/cms/js/app.js
@@ -2,16 +2,27 @@ import CMS from 'netlify-cms'
 import pages from './pages/index'
 import settings from './settings/index'
 
+let liam;
+console.log('The environment is: ', process.env.NODE_ENV);
+
+if (process.env.NODE_ENV == 'development') {
+    liam = {
+        name: 'proxy',
+        proxy_url: 'http://localhost:8081/api/v1',
+        branch: 'master'
+    }
+} else {
+    liam = {
+        name: 'git-gateway',
+        branch: 'master'
+    }
+};
+
 CMS.init({
     config: {
         load_config_file: false,
         local_backend: true,
-        backend: {
-            name: 'git-gateway',
-            // name: 'proxy',
-            // proxy_url: 'http://localhost:8081/api/v1',
-            branch: 'master'
-        },
+        backend: liam,
         media_folder: '/static/images/uploads',
         public_folder: '/images/uploads',
         collections: [pages, settings]
