de5bc314602b331b8729a3a0db42ba9087a96575
GotPop <liam@gotpop.net>
2021-06-08 16:45:01 +0100

    Restructure homepage ui

--- assets/cms/js/partials/sections/banner_page.js
+++ assets/cms/js/partials/sections/banner_page.js
@@ -1,5 +1,7 @@
 import linkPage from "../../fields/link_page";
+import iconSelect from "../../fields/select_icon";
 import title from "../../fields/title";
+import { lorumIpsumMedium } from "../data/loremIpsum";
 
 const bannerPage = {
     label: "Banner Page",
@@ -8,7 +10,15 @@ const bannerPage = {
     collapsed: false,
     fields: [
         title,
-        linkPage
+        {
+            label: "Supporting text",
+            name: "textSupporting",
+            widget: "string",
+            default: lorumIpsumMedium,
+            required: false
+        },
+        linkPage,
+        iconSelect
     ]
 };
 
